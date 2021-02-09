f4201f4367af70a24dedd958d22cdf4569b0a49e
YunHyeRim1 <0305_08@naver.com>
2021-02-09 15:38:32 +0900

    15:40

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,36 +1,44 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
+/**
+ * Handles requests for the application home page.
+ */
 @Controller
-@SessionAttributes({"ctx","bkg","cmm"})
+@SessionAttributes({"ctx","cmm","shw"})
 public class HomeController {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    @Autowired HttpSession session;
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired HttpSession session;
 	@Autowired HttpServletRequest request;
 
     @GetMapping("/")
     public String index(HttpSession session, HttpServletRequest request) {
     	String ctx = request.getContextPath();
     	session.setAttribute("ctx", ctx);
-    	session.setAttribute("bkg", session.getAttribute("ctx")+"/resources/bkg");
     	session.setAttribute("cmm", session.getAttribute("ctx")+"/resources/cmm");
-        logger.info("Hello. This is LogManager's logger");
+    	session.setAttribute("shw", session.getAttribute("ctx")+"/resources/shw");
+    	
+        logger.info("Project Initialized ... ");
+        
         return "index";
     }
-    @GetMapping("/move/bkg/{page}")
-    public String move(@PathVariable String page){
-        logger.info("이동경로>>> bkg/"+page);
-        return String.format("bkg:%s", page);
+ 
+    @GetMapping("/move/{dir}/{page}") 
+    public String move(@PathVariable String dir, 
+    						@PathVariable String page) {
+        logger.info("이동 디렉토리: " + dir);
+        logger.info("이동 파일: " + page);
+        return String.format("%s/%s", dir, page);
     }
+
 }
--- src/main/webapp/resources/cmm/js/cmm.js
+++ src/main/webapp/resources/cmm/js/cmm.js
@@ -1,24 +1,16 @@
 'use strict'
 var cmm = cmm || {}
 cmm = (() => {
-<<<<<<< HEAD
+
 	const init = x => { // add 페이지로 이동하는 거
         $('#add').click(e => {location.href=`${x}/move/shw/add`})
         $('#list').click(e => {location.href=`${x}/move/shw/list`})
 		$('#detail').click(e => {location.href=`${x}/move/shw/detail`})
-=======
-	const init = x => {
-        $('#list').click(e => {location.href=`${x}/move/bkg/list`})
-        $('#booking').click(e => {location.href=`${x}/move/bkg/booking`})
-		$('#insert-many-bkg').click(e => {
-			e.preventDefault()
-			$.getJSON(`${x}/bookings/insert-many/${$('#bkg-count').val()}`, d => {
-				alert(`${$('#bkg-count').val()}건 더미데이터 생성`)
-				location.reload()
-			})
-		})
->>>>>>> refs/heads/develop-keb
-	}
+		
+		}
+	/*const goHome = x => {
+		$(`#goHome`).click(e => {location.href=`${x}/`})
+	}*/
 	const goHome = x => {location.href = `${x}/`}
 	return { init, goHome }
 })()
